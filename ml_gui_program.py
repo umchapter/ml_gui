e3a967fee78f9a4d8a4106bfbd63a118858328df
umchapter <102334776+umchapter@users.noreply.github.com>
2022-05-26 10:02:29 +0900

    ml_gui

---
+++ ml_gui_program.py
@@ -0,0 +1,19 @@
+from PyQt5.QtWidgets import *
+import sys, pickle
+from PyQt5 import uic, QtWidgets ,QtCore, QtGui
+
+
+class UI(QMainWindow) :
+    def __init__(self) :
+        super(UI, self).__init__()
+        uic.loadUi('main_Window.ui', self)
+
+        # self.show()
+
+
+if __name__ == "__main__" :
+    app = QtWidgets.QApplication(sys.argv)
+    window = UI()
+    window.show()
+
+    sys.exit(app.exec_())
